6a404f0980e4acb72dff2fffd1385371ea3749dd
Danuka Dilshan Rathnayaka <154247403+DanukaDilshann@users.noreply.github.com>
2024-12-12 18:52:52 +0530

    Update Hugginface_config.py

--- Hugginface_config.py
+++ Hugginface_config.py
@@ -1,10 +1,12 @@
 from langchain_huggingface import HuggingFaceEndpoint
 import os
 from langchain.prompts import PromptTemplate
+from dotenv import load_dotenv
 
-sec_key = "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"
-os.environ['HUGGINGFACEHUB_API_TOKEN'] = sec_key
 
+load_dotenv()
+
+os.environ['HUGGINGFACEHUB_API_TOKEN'] = os.getenv("HUGGINGFACEHUB_API_TOKEN")
 repo_id = "microsoft/Phi-3-mini-4k-instruct"
 
 llm = HuggingFaceEndpoint(repo_id=repo_id,temperature=0.7, max_length=128)
